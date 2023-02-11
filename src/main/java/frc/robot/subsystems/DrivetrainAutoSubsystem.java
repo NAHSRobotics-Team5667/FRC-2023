d90956bde19e7899e41cfb45c7bcf98839881bcd
Liam O <liamloo12345678910@gmail.com>
2023-02-11 14:45:11 -0500

    Update DrivetrainAutoSubsystem.java

--- src/main/java/frc/robot/subsystems/DrivetrainAutoSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainAutoSubsystem.java
@@ -4,78 +4,12 @@
 
 package frc.robot.subsystems;
 
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.SPI.Port;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveConstants;
 
 /** Represents a swerve drive style drivetrain. */
-public class DrivetrainAutoSubsystem extends SubsystemBase {
-	public static final double kMaxSpeed = DriveConstants.kMaxSpeed; // 5 is 3 meters per second
-	public static final double kMaxAngularSpeed = DriveConstants.kMaxAngularSpeed; // 2 is 1/2 rotation per second
-	private static DutyCycleEncoder FREncoder = new DutyCycleEncoder(DriveConstants.FREncoderID);
-	private static DutyCycleEncoder FLEncoder = new DutyCycleEncoder(DriveConstants.FLEncoderID);
-	private static DutyCycleEncoder BREncoder = new DutyCycleEncoder(DriveConstants.BREncoderID);
-	private static DutyCycleEncoder BLEncoder = new DutyCycleEncoder(DriveConstants.BLEncoderID);
-	public static SwerveModule m_frontLeft = new SwerveModule(DriveConstants.kFrontLeftDriveID, DriveConstants.kFrontLeftTurningID, 0, FLEncoder);
-	public static SwerveModule m_frontRight = new SwerveModule(DriveConstants.kFrontRightDriveID, DriveConstants.kFrontRightTurningID, 0, FREncoder);
-	public static SwerveModule m_backLeft = new SwerveModule(DriveConstants.kBackLeftDriveID, DriveConstants.kBackLeftTurningID, 0, BLEncoder);
-	public static SwerveModule m_backRight = new SwerveModule(DriveConstants.kBackRightDriveID, DriveConstants.kBackRightTurningID, 0, BREncoder);
-	
-	private final SwerveModulePosition[] positions = {
-		DrivetrainAutoSubsystem.m_frontLeft.getPosition(), 
-		DrivetrainAutoSubsystem.m_frontRight.getPosition(), 
-		DrivetrainAutoSubsystem.m_backLeft.getPosition(), 
-		DrivetrainAutoSubsystem.m_backRight.getPosition()
-	};
-	
-	private static final Translation2d m_frontLeftLocation = new Translation2d(0.417, -0.417);
-	private final static Translation2d m_frontRightLocation = new Translation2d(0.417, 0.417);
-	private final static Translation2d m_backLeftLocation = new Translation2d(-0.417, -0.417);
-	private final static Translation2d m_backRightLocation = new Translation2d(-0.417, 0.417);
-	
-	public final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
-		m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation);
-
-	private final AHRS m_gyro = new AHRS(Port.kMXP);
-
-	public final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, m_gyro.getRotation2d(), positions);
-
+public class DrivetrainAutoSubsystem extends DrivetrainSubsystem {
 	public DrivetrainAutoSubsystem() {
-		m_gyro.reset();
-	}
-
-	/**
-	 * Method to drive the robot using joystick info.
-	 *
-	 * @param xSpeed        Speed of the robot in the x direction (forward).
-	 * @param ySpeed        Speed of the robot in the y direction (sideways).
-	 * @param rot           Angular rate of the robot.
-	 * @param fieldRelative Whether the provided x and y speeds are relative to the
-	 *                      field.
-	 */
-	
-	public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-		var swerveModuleStates = m_kinematics.toSwerveModuleStates(
-				fieldRelative
-						? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
-						: new ChassisSpeeds(xSpeed, ySpeed, rot));
-		SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
-		m_frontLeft.setDesiredState(swerveModuleStates[0]);
-		m_frontRight.setDesiredState(swerveModuleStates[1]);
-		m_backLeft.setDesiredState(swerveModuleStates[2]);
-		m_backRight.setDesiredState(swerveModuleStates[3]);
+		super();
 	}
 
     public void pleaseGodLetThisWork(SwerveModuleState Wheel1, SwerveModuleState Wheel2, SwerveModuleState Wheel3, SwerveModuleState Wheel4){
@@ -85,55 +19,6 @@ public class DrivetrainAutoSubsystem extends SubsystemBase {
 		m_backRight.setDesiredState(Wheel4);
     }
 
-    public Pose2d getPositionPose2d(){
-        return m_odometry.getPoseMeters();
-    }
-    public void resetPose(Rotation2d gyro, SwerveModulePosition[] positions, Pose2d m_pose){
-        // m_odometry.resetPosition(null, m_swerveModuleFakeStates, m_pose);
-        m_odometry.resetPosition(gyro, positions, m_pose);
-    }
-	public void driveVoltage(double voltage) {
-		m_frontLeft.driveVoltage(voltage);
-		m_frontRight.driveVoltage(voltage);
-		m_backLeft.driveVoltage(voltage);
-		m_backRight.driveVoltage(voltage);
-	}
+	
 
-	/** Updates the field relative position of the robot. */
-	public void updateOdometry() {
-		m_odometry.update(
-				m_gyro.getRotation2d(),
-				positions);
-	}
-
-
-
-	@Override
-	public void periodic() {
-		//public int checkBumper
-		SmartDashboard.putNumber("Gyro", m_gyro.getAngle());
-		//SmartDashboard.putNumber("Abs Encoder, back left edition", BackLeft.getAbsolutePosition());
-		SmartDashboard.putNumber("FRA-Actual", m_frontRight.getTurnEncoderDistance());
-		SmartDashboard.putNumber("FLA-Actual", m_frontLeft.getTurnEncoderDistance());
-		SmartDashboard.putNumber("BRA-Actual", m_backRight.getTurnEncoderDistance());
-		SmartDashboard.putNumber("BLA-Actual", m_backLeft.getTurnEncoderDistance());
-
-		
-		SmartDashboard.putNumber("FRA-Setpoint", m_frontRight.getAngleSetpoint());
-		SmartDashboard.putNumber("FLA-Setpoint", m_frontLeft.getAngleSetpoint());
-		SmartDashboard.putNumber("BRA-Setpoint", m_backRight.getAngleSetpoint());
-		SmartDashboard.putNumber("BLA-Setpoint", m_backLeft.getAngleSetpoint());
-		
-
-		SmartDashboard.putNumber("FRD-Actual", m_frontRight.getDriveEncoderDistance());
-		SmartDashboard.putNumber("FLD-Actual", m_frontLeft.getDriveEncoderDistance());
-		SmartDashboard.putNumber("BRD-Actual", m_backRight.getDriveEncoderDistance());
-		SmartDashboard.putNumber("BLD-Actual", m_backLeft.getDriveEncoderDistance());
-
-		SmartDashboard.putNumber("FRD-Setpoint", m_frontRight.getDriveSetpoint());
-		SmartDashboard.putNumber("FLD-Setpoint", m_frontLeft.getDriveSetpoint());
-		SmartDashboard.putNumber("BRD-Setpoint", m_backRight.getDriveSetpoint());
-		SmartDashboard.putNumber("BLD-Setpoint", m_backLeft.getDriveSetpoint());
-		
-	}
 }
