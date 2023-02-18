2e481f150a8163e5875f627a44b7aae281259c40
bennysans <46729245+bennysans@users.noreply.github.com>
2023-02-18 14:26:30 -0500

    guys im on the right account now omg

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,7 +59,7 @@ public class RobotContainer {
 		// This will load the file "FullAuto.path" and generate it with a max velocity
 		// of 4 m/s and a max acceleration of 3 m/s^2
 		// for every path in the group
-		PathPlannerTrajectory pathGroup = PathPlanner.loadPath("Die", new PathConstraints(2, .5));
+		PathPlannerTrajectory pathGroup = PathPlanner.loadPath("Die", new PathConstraints(.2, .02));
 
 		// This is just an example event map. It would be better to have a constant,
 		// global event map
@@ -98,7 +98,7 @@ public class RobotContainer {
 				poseSupplier, // Pose2d supplier
 				resetPoseConsumer, // Pose2d consumer, used to reset odometry at the beginning of auto
 				m_drive.m_kinematics, // SwerveDriveKinematics
-				new PIDConstants(8, 05, .7), // PID constants to correct for translation error (used to create the X
+				new PIDConstants(12.2, 0, .7), // PID constants to correct for translation error (used to create the X
 													// and Y PID controllers)
 				new PIDConstants(0.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the
 													// rotation controller)
@@ -106,7 +106,7 @@ public class RobotContainer {
 				eventMap,
 				true, // Should the path be automatically mirrored depending on alliance color.
 						// Optional, defaults to true
-				m_drive // The drive subsystem. Used to properly set the requirements of path following
+			m_drive // The drive subsystem. Used to properly set the requirements of path following
 						// commands
 		);
 
